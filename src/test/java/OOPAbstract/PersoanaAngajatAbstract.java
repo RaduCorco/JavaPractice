b2122a2d431bbb7654e3e695ce39069e24cf7282
RaduCorco <raducorcoveanu@gmail.com>
2022-06-02 19:20:40 +0300

    am invatat despre abstractizare

---
+++ src/test/java/OOPAbstract/PersoanaAngajatAbstract.java
@@ -0,0 +1,78 @@
+package OOPAbstract;
+
+public class PersoanaAngajatAbstract extends PersoanaAbstract {
+
+    private String nume;
+    private String prenume;
+    private String meserie;
+
+    public PersoanaAngajatAbstract(String nume, String prenume, String meserie) {
+        this.nume = nume;
+        this.prenume = prenume;
+        this.meserie = meserie;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+
+    public String getMeserie() {
+        return meserie;
+    }
+
+    public void setMeserie(String meserie) {
+        this.meserie = meserie;
+    }
+
+    @Override
+    void Munceste() {
+
+    }
+
+    @Override
+    void munceste() {
+
+    }
+
+    @Override
+    void primesteSalar() {
+
+    }
+
+    @Override
+    void Invata() {
+
+    }
+
+    @Override
+    void primestebursa() {
+
+    }
+
+    @Override
+    void Primestebursa() {
+
+    }
+
+    @Override
+    void Someaza() {
+
+    }
+
+    @Override
+    void PrimesteAjutor() {
+
+    }
+}
